ff27617ff489a34f8b6de7f95a43f148761464a9
carmaa786 <carmaa786@gmail.com>
2025-07-10 05:48:11 +0000

    added city

--- js/location-redirect.js
+++ js/location-redirect.js
@@ -5,13 +5,13 @@ fetch('https://ipinfo.io/json?token=971e67675db5c4')
   .then(data => {
     console.log("City detected:", data.city);
     const cityMap = {
-       "new delhi": "delhi",
-      "delhi": "delhi",
-      "dehradun": "dehradun",
-      "gurgaon": "gurgaon",
-      "noida": "noida",
-      "alighar": "alighar",
-      "haridwar": "haridwar"
+        "new delhi": "delhi",
+        "delhi": "delhi",
+        "dehradun": "dehradun",
+        "gurgaon": "gurgaon",
+        "noida": "noida",
+        "alighar": "alighar",
+        "haridwar": "haridwar"
     };
 
     const userCity = data.city.toLowerCase().trim();
